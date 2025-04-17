1e1e478b3b1f117dfd065c2838b217a440c6060d
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2025-04-17 18:46:42 +0300

    Version 1.00 Released

--- main.py
+++ main.py
@@ -1,78 +1,102 @@
 from playsound import playsound
+from flask import Flask,render_template, request, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
 import time
 
 
-translate_dic={
-    'A':'._',
-    'B':'_...',
-    'C':'_._.',
-    'D':'_..',
-    'E':'.',
-    'F':'.._.',
-    'G':'__.',
-    'H':'....',
-    'I':'..',
-    'J':'.___',
-    'K':'_._',
-    'L':'._..',
-    'M':'__',
-    'N':'_.',
-    'O':'___',
-    'P':'.__.',
-    'Q':'__._',
-    'R':'._.',
-    'S':'...',
-    'T':'_',
-    'U':'.._',
-    'V':'..._',
-    'W':'.__',
-    'X':'_.._',
-    'Y':'_.__',
-    'Z':'__..',
-    '0':'_____',
-    '1':'.____',
-    '2':'..___',
-    '3':'...__',
-    '4':'...._',
-    '5':'.....',
-    '6':'_....',
-    '7':'__...',
-    '8':'___..',
-    '9':'____.',
-    ' ':'/',
+app=Flask(__name__)
+
+app.config['SECRET_KEY']= "NEW"
+
+class UserInput(FlaskForm):
+    user_input=StringField("Enter Your Text: ", default="")
+    morse_input=StringField("Morse Input", default="")
+    submit=SubmitField("Submit")
+
+
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    form=UserInput()
+    user_input = form.user_input.data
+    translate_dic={
+        'A':'._','B':'_...','C':'_._.',
+        'D':'_..','E':'.', 'F':'.._.',
+        'G':'__.','H':'....', 'I':'..','J':'.___',
+        'K':'_._', 'L':'._..','M':'__','N':'_.', 'O':'___',
+        'P':'.__.', 'Q':'__._','R':'._.', 'S':'...',
+        'T':'_', 'U':'.._', 'V':'..._','W':'.__','X':'_.._',
+        'Y':'_.__','Z':'__..', '0':'_____', '1':'.____',
+        '2':'..___', '3':'...__','4':'...._','5':'.....',
+        '6':'_....', '7':'__...', '8':'___..','9':'____.',
+        ' ':'/',
+         }
     
-}
+    user_input = " ".join(translate_dic[c] for c in user_input.upper())
+    output=user_input
 
-message = input("Enter Your Text: ")
-message= " ".join(translate_dic[c] for c in message.upper())
+    def play_sound(user_input):
+        for c in user_input:
+            if c==".":
+                playsound('resourse/short.wav')
+                time.sleep(0.001)
 
-print(message)
+            elif c=="_" :
+                playsound('resourse/long.wav')
+                time.sleep(0.001)
 
+            elif c==" " or "/" :
+                time.sleep(0.01)
 
-def play_sound(message):
-    for c in message:
-        if c==".":
-            playsound('resourse/short.wav')
-            time.sleep(0.001)
+            else:
+                return "There was an Error...Sorry try again please!" 
+    play_sound(user_input)            
 
-        elif c=="_" :
-            playsound('resourse/long.wav')
-            time.sleep(0.001)
-
-        elif c==" " or "/" :
-            time.sleep(0.01)
-
-        else:
-            print("There was an Error...")
-
-
-play_sound(message)            
+    
+    return render_template("index.html", form=form,output=output)
 
 
-reverse_dic={v:k for k,v in translate_dic.items()}
 
-nw_message=input("Enter Your Morse Code: ")
+#From Morse code to Text
+@app.route("/Morse-Converter", methods=['POST', 'GET'])
+def morse():
+    form=UserInput()
+    morse_input = form.morse_input.data
+    translate_dic={
+        'A':'._','B':'_...','C':'_._.',
+        'D':'_..','E':'.', 'F':'.._.',
+        'G':'__.','H':'....', 'I':'..','J':'.___',
+        'K':'_._', 'L':'._..','M':'__','N':'_.', 'O':'___',
+        'P':'.__.', 'Q':'__._','R':'._.', 'S':'...',
+        'T':'_', 'U':'.._', 'V':'..._','W':'.__','X':'_.._',
+        'Y':'_.__','Z':'__..', '0':'_____', '1':'.____',
+        '2':'..___', '3':'...__','4':'...._','5':'.....',
+        '6':'_....', '7':'__...', '8':'___..','9':'____.',
+        ' ':'/','':''
+         }
 
-morse_message= "".join(reverse_dic[c] for c in nw_message.split(" "))
+    reverse_dic={v:k for k,v in translate_dic.items()}
+    try:
+     morse_message= "".join(reverse_dic[c] for c in morse_input.split(" "))
 
-print(morse_message)
+     text_output=morse_message
+    except KeyError:
+        error_message="Invalid Input!!! Please Enter valid Morse code"
+        return error_message
+
+
+    return render_template("morse.html",form=form, text_output=text_output)
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)   
