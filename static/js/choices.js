444ecade35334b9f7f535b22860483c525bb5c07
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2025-04-29 12:36:42 -0700

    flask integrated

---
+++ static/js/choices.js
@@ -0,0 +1,11 @@
+const choices = new Choices('[data-trigger]',
+    {
+      searchEnabled: false,
+      itemSelectText: '',
+    });
+    
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'UA-23581568-13');
